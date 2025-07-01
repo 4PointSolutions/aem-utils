3295d7208472166d96865d4bf4cbf5a44099640c
Rob McDougall <rmcdougawww@rogers.com>
2025-07-01 09:27:13 -0400

    🎉 Create initial application

---
+++ aem_cntrl/src/main/java/com/_4point/aem/aem_utils/aem_cntrl/AemCntrlApplication.java
@@ -0,0 +1,13 @@
+package com._4point.aem.aem_utils.aem_cntrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AemCntrlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AemCntrlApplication.class, args);
+  }
+
+}
---
+++ aem_cntrl/src/test/java/com/_4point/aem/aem_utils/aem_cntrl/AemCntrlApplicationTest.java
@@ -0,0 +1,14 @@
+package com._4point.aem.aem_utils.aem_cntrl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AemCntrlApplicationTest {
+
+	  @Test
+	  void contextLoads() {
+	  }
+}
