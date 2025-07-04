3e67e5384e09507afd6e18efccf32f48722b7f67
Rob McDougall <rmcdougawww@rogers.com>
2025-07-04 09:35:18 -0400

    ✨ Call the installer code from the install command

--- aem_cntrl/src/main/java/com/_4point/aem/aem_utils/aem_cntrl/commands/InstallCommand.java
+++ aem_cntrl/src/main/java/com/_4point/aem/aem_utils/aem_cntrl/commands/InstallCommand.java
@@ -1,15 +1,25 @@
 package com._4point.aem.aem_utils.aem_cntrl.commands;
 
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
+
+import com._4point.aem.aem_utils.aem_cntrl.domain.OperatingSystem;
+import com._4point.aem.aem_utils.aem_cntrl.domain.ports.api.AemInstaller;
 
 import picocli.CommandLine.Command;
 
 @Command(name = "install")
 public class InstallCommand implements Callable<Integer> {
+	
+	private final AemInstaller aemInstaller;
+	
+	public InstallCommand(AemInstaller aemInstaller) {
+		this.aemInstaller = aemInstaller;
+	}
 
 	@Override
 	public Integer call() throws Exception {
-		// TODO Auto-generated method stub
+		aemInstaller.installAem(OperatingSystem.isWindows() ? Path.of("\\Adobe") : Path.of("/opt/adobe"), Path.of(""));
 		return 0;
 	}
 
