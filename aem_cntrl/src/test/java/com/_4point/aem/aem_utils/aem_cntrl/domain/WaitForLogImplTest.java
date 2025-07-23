0cdc1fd03c44195d0ace00ca461cf1c618314c43
robm <robm@LAPTOP-EL91E6U4.localdomain>
2025-07-23 07:40:17 -0400

    ?? Fixed flakey test - failed on Linux, passed on Windows
    It was relying on a Windows-specific file path, so I moved that path
    into a separate test that does not run on Linux.

--- aem_cntrl/src/test/java/com/_4point/aem/aem_utils/aem_cntrl/domain/WaitForLogImplTest.java
+++ aem_cntrl/src/test/java/com/_4point/aem/aem_utils/aem_cntrl/domain/WaitForLogImplTest.java
@@ -7,19 +7,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import com._4point.aem.aem_utils.aem_cntrl.domain.WaitForLogImpl.AemDirType;
 
 class WaitForLogImplTest {
 
 	@ParameterizedTest
-	@CsvSource({ "/Adobe, ABSOLUTE", "./Adobe, RELATIVE", "../Adobe, RELATIVE", "Adobe, DEFAULT", "C:\\Adobe, ABSOLUTE", ",NULL" })
+	@CsvSource({ "/Adobe, ABSOLUTE", "./Adobe, RELATIVE", "../Adobe, RELATIVE", "Adobe, DEFAULT", ",NULL" })
 	void testAemDirType_of(Path aemDir, AemDirType expectedType) {
 		assertEquals(expectedType, AemDirType.of(aemDir));
+	}
+
+	@EnabledOnOs(OS.WINDOWS)
+	@Test
+	void testAemDirType_of_windows_withDriveLetter() {
+		Path aemDir = Path.of("C:\\Adobe");
+		assertEquals(AemDirType.ABSOLUTE, AemDirType.of(aemDir));
 	}
 
 	@Test
