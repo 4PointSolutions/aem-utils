0a9aa75b73b07497ff8a1f62b8fdbebffb1bfb2f
Rob McDougall <rmcdougawww@rogers.com>
2025-08-03 08:13:20 -0400

    🔊 Added log messages for waitForLog

--- aem_cntrl/src/main/java/com/_4point/aem/aem_utils/aem_cntrl/domain/WaitForLogImpl.java
+++ aem_cntrl/src/main/java/com/_4point/aem/aem_utils/aem_cntrl/domain/WaitForLogImpl.java
@@ -5,12 +5,19 @@ import java.time.Duration;
 import java.util.function.BiFunction;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com._4point.aem.aem_utils.aem_cntrl.domain.AemFiles.AemDir;
 import com._4point.aem.aem_utils.aem_cntrl.domain.AemFiles.LogFile;
 import com._4point.aem.aem_utils.aem_cntrl.domain.ports.api.WaitForLog;
+import com._4point.aem.aem_utils.aem_cntrl.domain.ports.api.WaitForLog.RegexArgument.RegexCustom;
+import com._4point.aem.aem_utils.aem_cntrl.domain.ports.api.WaitForLog.RegexArgument.RegexShutdown;
+import com._4point.aem.aem_utils.aem_cntrl.domain.ports.api.WaitForLog.RegexArgument.RegexStartup;
 import com._4point.aem.aem_utils.aem_cntrl.domain.ports.spi.Tailer.TailerFactory;
 
 public class WaitForLogImpl implements WaitForLog {
+	private static final Logger log = LoggerFactory.getLogger(WaitForLogImpl.class);
 	
 	private final AemDir aemDir;
 	private final TailerFactory tailerFactory;
@@ -36,8 +43,10 @@ public class WaitForLogImpl implements WaitForLog {
 	public void waitForLog(RegexArgument regexArgument, Duration timeout, FromOption fromOption, final Path unqualifiedAemDir) {
 		Path finalAemDir = aemDir.toQualified(unqualifiedAemDir);									// find the aem directory
 		LogFile logFile = logFileFactory.apply(finalAemDir, tailerFactory);							// locate the log file and create the log file object
+		log.info("Waiting for {} log entry in log {} within {}", toString(regexArgument), logFile.toString(), timeout);
 		logFile.monitorLogFile(toPattern(regexArgument), timeout, toLogFileFromOption(fromOption))	// monitor the log file for a matching entry, throw an exception if not found
 			   .orElseThrow(() -> new WaitForLogException("No log entry matching " + toPattern(regexArgument) + " found in " + logFile.toString() + " within " + timeout));
+		log.info("Found {} log entry in {}", toString(regexArgument), logFile.toString());
 	}
 
 	private LogFile.FromOption toLogFileFromOption(FromOption from) {
@@ -55,6 +64,11 @@ public class WaitForLogImpl implements WaitForLog {
 		};
 	}
 
-	
-
+	private String toString(RegexArgument regexArgument) {
+         return switch (regexArgument) {
+             case RegexStartup r -> "Startup";
+             case RegexShutdown r -> "Shutdown";
+             case RegexCustom r -> "Custom Regex: " + r.regex();
+         };
+     }
 }
