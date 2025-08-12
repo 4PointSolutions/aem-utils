253696df221474844e6ea6d2770f33527d947a5b
Rob McDougall <rmcdougawww@rogers.com>
2025-08-12 09:14:07 -0400

    ✅ Updated test to include verification of mock calls

--- aem_cntrl/src/test/java/com/_4point/aem/aem_utils/aem_cntrl/commands/WaitForLogCommandTests.java
+++ aem_cntrl/src/test/java/com/_4point/aem/aem_utils/aem_cntrl/commands/WaitForLogCommandTests.java
@@ -13,11 +13,14 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import com._4point.aem.aem_utils.aem_cntrl.domain.DefaultsImpl;
 import com._4point.aem.aem_utils.aem_cntrl.domain.ports.api.WaitForLog;
+import com._4point.aem.aem_utils.aem_cntrl.domain.ports.api.WaitForLog.RegexArgument;
 
 import picocli.CommandLine;
 
 class WaitForLogCommandTests {
+	private static final Path AEM_DIR = DefaultsImpl.aemDir();
 
 	@SpringBootTest(args = {"wflog"})
 	static class WaitForLogCommandNoArgsTest {
@@ -43,6 +46,8 @@ class WaitForLogCommandTests {
 		void testWaitForLog_Startup() throws Exception {
 			doNothing().when(waitForLogMock).waitForLog(WaitForLog.RegexArgument.startup(), WaitForLog.DEFAULT_DURATION,
 					WaitForLog.DEFAULT_FROM_OPTION, null);
+			verify(waitForLogMock).waitForLog(eq(WaitForLog.RegexArgument.startup()), eq(WaitForLog.DEFAULT_DURATION),
+					eq(WaitForLog.DEFAULT_FROM_OPTION), eq(AEM_DIR));
 			assertEquals(CommandLine.ExitCode.OK, exitCodeGenerator.getExitCode(), "Exit code should be 0 for successful installation with --startup argument.");
 		}
 	}
@@ -57,6 +62,8 @@ class WaitForLogCommandTests {
 		void testWaitForLog_Shutdown() throws Exception {
 			doNothing().when(waitForLogMock).waitForLog(WaitForLog.RegexArgument.shutdown(), WaitForLog.DEFAULT_DURATION,
 					WaitForLog.DEFAULT_FROM_OPTION, null);
+			verify(waitForLogMock).waitForLog(eq(WaitForLog.RegexArgument.shutdown()), eq(WaitForLog.DEFAULT_DURATION),
+					eq(WaitForLog.DEFAULT_FROM_OPTION), eq(AEM_DIR));
 			assertEquals(CommandLine.ExitCode.OK, exitCodeGenerator.getExitCode(), "Exit code should be 0 for successful installation with --startup argument.");
 		}
 	}
@@ -70,8 +77,11 @@ class WaitForLogCommandTests {
 		@Test
 		void testWaitForLog_CustomRegex() throws Exception {
 			Pattern regex = Pattern.compile(".*Error.*");
-			doNothing().when(waitForLogMock).waitForLog(WaitForLog.RegexArgument.custom(regex), WaitForLog.DEFAULT_DURATION,
+			RegexArgument expectedRegexArg = WaitForLog.RegexArgument.custom(regex);
+			doNothing().when(waitForLogMock).waitForLog(expectedRegexArg, WaitForLog.DEFAULT_DURATION,
 					WaitForLog.DEFAULT_FROM_OPTION, null);
+			verify(waitForLogMock, times(1)).waitForLog(any(WaitForLog.RegexArgument.RegexCustom.class)/* For some reason, can't use eq() here */, 
+					eq(WaitForLog.DEFAULT_DURATION), eq(WaitForLog.DEFAULT_FROM_OPTION), eq(AEM_DIR));
 			assertEquals(CommandLine.ExitCode.OK, exitCodeGenerator.getExitCode(), "Exit code should be 0 for successful installation with --startup argument.");
 		}
 	}
@@ -99,6 +109,8 @@ class WaitForLogCommandTests {
 		void testWaitForLog_FromStart() throws Exception {
 			doNothing().when(waitForLogMock).waitForLog(WaitForLog.RegexArgument.startup(), WaitForLog.DEFAULT_DURATION,
 					WaitForLog.FromOption.START, null);
+			verify(waitForLogMock).waitForLog(eq(WaitForLog.RegexArgument.startup()), eq(WaitForLog.DEFAULT_DURATION),
+					eq(WaitForLog.FromOption.START), eq(AEM_DIR));
 			assertEquals(CommandLine.ExitCode.OK, exitCodeGenerator.getExitCode(), "Exit code should be 0 for successful installation with --startup argument.");
 		}
 	}
@@ -113,6 +125,8 @@ class WaitForLogCommandTests {
 		void testWaitForLog_FromEnd() throws Exception {
 			doNothing().when(waitForLogMock).waitForLog(WaitForLog.RegexArgument.startup(), WaitForLog.DEFAULT_DURATION,
 					WaitForLog.FromOption.END, null);
+			verify(waitForLogMock).waitForLog(eq(WaitForLog.RegexArgument.startup()), eq(WaitForLog.DEFAULT_DURATION),
+					eq(WaitForLog.FromOption.END), eq(AEM_DIR));
 			assertEquals(CommandLine.ExitCode.OK, exitCodeGenerator.getExitCode(), "Exit code should be 0 for successful installation with --startup argument.");
 		}
 	}
@@ -127,6 +141,8 @@ class WaitForLogCommandTests {
 		void testWaitForLog_Timeout() throws Exception {
 			doNothing().when(waitForLogMock).waitForLog(WaitForLog.RegexArgument.startup(), Duration.ofMinutes(10),
 					WaitForLog.DEFAULT_FROM_OPTION, null);
+			verify(waitForLogMock).waitForLog(eq(WaitForLog.RegexArgument.startup()), eq(Duration.ofMinutes(10)),
+					eq(WaitForLog.DEFAULT_FROM_OPTION), eq(AEM_DIR));
 			assertEquals(CommandLine.ExitCode.OK, exitCodeGenerator.getExitCode(), "Exit code should be 0 for successful installation with valid --timeout argument.");
 		}
 	}
@@ -167,6 +183,8 @@ class WaitForLogCommandTests {
 		void testWaitForLog_AemDirectory() throws Exception {
 			doNothing().when(waitForLogMock).waitForLog(WaitForLog.RegexArgument.startup(), WaitForLog.DEFAULT_DURATION,
 					WaitForLog.DEFAULT_FROM_OPTION, Path.of("AemDir"));
+			verify(waitForLogMock).waitForLog(eq(WaitForLog.RegexArgument.startup()), eq(WaitForLog.DEFAULT_DURATION),
+					eq(WaitForLog.DEFAULT_FROM_OPTION), eq(Path.of("AemDir")));
 			assertEquals(CommandLine.ExitCode.OK, exitCodeGenerator.getExitCode(), "Exit code should be 0 for successful installation with --aemdir argument." );
 		}
 	}
